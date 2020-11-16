64f2a0d34c64f9614e2019d200cf324ae85f2721
ratan900 <52387727+ratan900@users.noreply.github.com>
2020-11-16 18:46:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'TOPSIS_RATAN_101803156',         # How you named your package folder (MyLib)
   packages = ['TOPSIS_RATAN_101803156'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'RATAN KUMAR',                   # Type in your name
